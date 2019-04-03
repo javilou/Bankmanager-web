4225aec87ab69c5b91f34fe3130168f3360446a1
Rodrigo <rodrigobreton98@gmail.com>
2019-04-03 09:29:32 +0200

    Todo correcto hasta el punto 17 (No incluido)

--- src/app/main/accounts/accounts-routing.module.ts
+++ src/app/main/accounts/accounts-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { BranchesHomeComponent } from './branches-home/branches-home.component';
+import { AccountsHomeComponent } from './accounts-home/accounts-home.component';
 
 const routes: Routes = [{
   path:'',
-  component: BranchesHomeComponent
+  component: AccountsHomeComponent
 }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class BranchesRoutingModule { }
+export class AccountsRoutingModule { }
--- src/app/main/accounts/accounts.module.ts
+++ src/app/main/accounts/accounts.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OntimizeWebModule } from 'ontimize-web-ngx';
-import { BranchesRoutingModule } from './branches-routing.module';
-import { BranchesHomeComponent } from './branches-home/branches-home.component';
+import { AccountsRoutingModule } from './accounts-routing.module';
+import { AccountsHomeComponent } from './accounts-home/accounts-home.component';
 
 @NgModule({
   imports: [
     CommonModule,
     OntimizeWebModule,
-    BranchesRoutingModule
+    AccountsRoutingModule
   ],
   declarations: [
-    BranchesHomeComponent
+    AccountsHomeComponent
   ]
 })
-export class BranchesModule { }
+export class AccountsModule { }
