4a4c5ac8770690be7b5bdefd31ed4c5933e5c60c
Rodrigo <rodrigobreton98@gmail.com>
2019-04-03 11:15:32 +0200

    todo correcto hasta el 18 excepto el metodo initialize() que estaba deprecated y fue eliminado

--- src/app/main/accounts/accounts-home/accounts-home.component.ts
+++ src/app/main/accounts/accounts-home/accounts-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { intRateMonthlyFunction } from '../../../shared/shared.module';
 
 @Component({
   selector: 'app-accounts-home',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountsHomeComponent implements OnInit {
 
-  constructor() { }
+ public intRateMonthly = intRateMonthlyFunction;
+
+ constructor() { }
 
   ngOnInit() {
   }
