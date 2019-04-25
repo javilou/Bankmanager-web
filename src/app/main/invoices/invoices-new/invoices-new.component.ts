d9d8a507fd46bcd020a408beb14253bc9b148848
Rodrigo <rodrigobreton98@gmail.com>
2019-04-25 13:02:03 +0200

    El input-file de invoices-new ya sube archivos y los guarda en una carpeta en c:/

--- src/app/main/invoices/invoices-new/invoices-new.component.ts
+++ src/app/main/invoices/invoices-new/invoices-new.component.ts
@@ -9,6 +9,26 @@ export class InvoicesNewComponent implements OnInit {
 
   constructor() { }
 
+  onUploadFile(event) {
+    if (event.target.files.lenght == 0) {
+      console.log("----- NO SE HA SELECCIONADO NINGUN ARCHIVO -----");
+    } else {
+      let file: File = event.target.files[0];
+      console.log("----- SE HA SELECCIONADO UN ARCHIVO -----");
+    }
+    for (var i = 0; i < event.target.files.length; i++) { 
+      var name = event.target.files[i].name;
+      var type = event.target.files[i].type;
+      var size = event.target.files[i].size;
+      var modifiedDate = event.target.files[i].lastModifiedDate;
+      
+      console.log ('Name: ' + name + "\n" + 
+        'Type: ' + type + "\n" +
+        'Last-Modified-Date: ' + modifiedDate + "\n" +
+        'Size: ' + Math.round(size / 1024) + " KB");
+    }
+  }
+
   ngOnInit() {
   }
 
